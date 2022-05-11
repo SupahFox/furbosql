106959379cb464987caa22905d51118d58fcc94f
Fabri <31490939+SupahFox@users.noreply.github.com>
2022-05-11 13:01:07 -0300

    Add files via upload

--- Furbo.sql
+++ Furbo.sql
@@ -20,9 +20,8 @@ Apellido varchar(50),
 Fecha_de_nacimiento datetime,
 Nacionalidad varchar(50),
 Posicion varchar(50)
- );
+);
  
- insert into jugador(CI_jug,Cod_jug,nombre)  value ("Santi") ;
 create table arbitro(
 CI_ar int,
 Cod_ar int,
@@ -45,9 +44,11 @@ foreign key (CI) references arbitro(CI_ar)
 
 create table cuerpo_tecnico(
 cod_ct int primary key,
-rol varchar(50)
+rol varchar(50),
+nombre varchar(50),
+apellido varchar(50)
 );
- 
+
 create table estadio(
 Cod_Estadio int primary key,
 Nombre varchar(50),
@@ -71,6 +72,7 @@ primary key (Cod_Seleccion1, Cod_Seleccion2),
 foreign key(Cod_Seleccion1)references Seleccion(Cod_Seleccion),
 foreign key(Cod_Seleccion2)references Seleccion(Cod_Seleccion)
 );
+
 create table jug_Partido(
 Cod_Jug int,
 Cod_sel int,
@@ -80,15 +82,23 @@ foreign key(Cod_sel)references Seleccion(Cod_Seleccion),
 foreign key(Cod_sel2)references Seleccion(Cod_Seleccion),
 foreign key(Cod_Jug)references Jugador(CI_jug)
 );
- 
+
 create table juega(
 cod_sel1 int,
 cod_sel2 int,
-cod_jug int,
-primary key(cod_sel1,cod_sel2,cod_jug),
+CI_jug int,
+primary key(cod_sel1,cod_sel2,CI_jug),
 foreign key(Cod_sel1)references Seleccion(Cod_Seleccion),
 foreign key(Cod_sel2)references Seleccion(Cod_Seleccion),
-foreign key(Cod_Jug)references Jugador(CI_jug)
+foreign key(CI_Jug)references jugador(CI_jug)
+);
+
+create table jugadPerteSel(
+cod_jug int,
+cod_sel int,
+primary key(cod_jug, cod_sel),
+constraint fk_2 foreign key (cod_jug) references jugador(ci_jug),
+constraint fk_1 foreign key (cod_sel) references seleccion(Cod_Seleccion)
 );
 
 create table participa(
@@ -129,33 +139,34 @@ primary key(cod_tec, cod_sel1, cod_sel2),
 
 foreign key(cod_sel1)references seleccion(Cod_seleccion),
 foreign key(cod_sel2)references seleccion(Cod_seleccion),
-foreign key(cod_tec)references cuerpo_tecnico(cod_ct) 
+foreign key(cod_tec)references cuerpo_tecnico(cod_ct)
 );
-use mundial;
 
-select jug.nombre, 'Jugador' Rol, sel1.nombre, sel2.nombre, ingj.fecha  from juega j
+create table cuerpTecPart(
+cod_ct int,
+cod_sel int,
+primary key(cod_ct, cod_sel),
+foreign key(cod_ct) references cuerpo_tecnico(cod_ct),
+foreign key(cod_sel) references seleccion(Cod_seleccion)
+);
+
+select jug.nombre, 'Jugador' Rol, sel1.nombre Local, sel2.nombre Visitante from juega jugar
 join ingresa_jug ingj
 join jugador jug
 join seleccion sel1
 join seleccion sel2
-on j.cod_jug = jug.CI_jug
+on jugar.CI_jug = jug.CI_jug
 where sel1.nombre = "argentina" and sel2.nombre= "uruguay"
-union select ar.nombre, 'Arbitro' Rol, sel1.nombre, sel2.nombre, ingt.fecha from arbitro
+union select ar.nombre, 'Arbitro' Rol, sel1.nombre Local, sel2.nombre Visitante from arbitro
 join ingresa_tec ingt
 join arbitro ar
 join seleccion sel1
 join seleccion sel2
-on ar.CI_ar = ar.cod_ar
-;
+on ar.CI_ar = ar.cod_ar;
 
-
-select CI_ar from arbitro 
-join seleccion ON arbitro.Nacionalidad = seleccion.Nombre
-union select * from cuerpo_tecnico
-join seleccion ON cuerpo_tecnico.
- 
-
-
-
-drop database mundial;
- 
+select jug.nombre Nombre, jug.apellido Apellido, 'Jugadores' Rol, sel.nombre Seleccion from jugador jug
+join jugadPerteSel jps
+join Seleccion sel where jps.cod_sel = sel.Cod_Seleccion and jug.CI_jug = jps.cod_jug
+union select ct.nombre Nombre, ct.apellido Apellido, 'Tecnicos' Rol, sel.nombre Seleccion from cuerpo_tecnico ct
+join cuerpTecPart ctp
+join Seleccion sel where ctp.cod_sel = sel.Cod_Seleccion and ct.cod_ct = ctp.cod_ct;
